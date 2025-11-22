870266bf17efa2d1b7e850d7d4af3a633eee166e
TRUPTIBUGADIKATTE82 <truptibugadikatte@gmail.com>
2025-11-22 18:50:50 +0530

    Add files via upload

---
+++ LoginController.java
@@ -0,0 +1,20 @@
+package com.example.Security1;
+import org.springframework.security.core.Authentication; 
+import org.springframework.stereotype.Controller; 
+import org.springframework.ui.Model; 
+import org.springframework.web.bind.annotation.GetMapping; 
+ 
+@Controller 
+public class LoginController { 
+ 
+    @GetMapping("/login") 
+    public String login() { 
+        return "login";  
+    } 
+ 
+    @GetMapping("/welcome") 
+    public String welcome(Model model, Authentication authentication) { 
+        model.addAttribute("username", authentication.getName()); 
+        return "welcome"; 
+    }
+}
---
+++ SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.example.Security1;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	 @Bean 
+	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { 
+	        http 
+	                .authorizeHttpRequests(auth -> auth 
+	                        .requestMatchers("/login", "/css/**").permitAll() 
+	                        .anyRequest().authenticated() 
+	                ) 
+	                .formLogin(form -> form 
+	                        .loginPage("/login")           
+	                        .defaultSuccessUrl("/welcome", true)
+
+	                        .permitAll() 
+	                ) 
+	                .logout(logout -> logout.permitAll()); 
+	 
+	        return http.build(); 
+	    } 
+  @Bean 
+   public UserDetailsService users(PasswordEncoder encoder) { 
+        return new InMemoryUserDetailsManager( 
+                User.withUsername("user") 
+                        .password(encoder.encode("password")) 
+                        .roles("USER") 
+                        .build(), 
+                User.withUsername("admin") 
+                        .password(encoder.encode("admin123")) 
+                        .roles("ADMIN") 
+                        .build() 
+        ); 
+    } 
+ 
+    @Bean 
+    public PasswordEncoder passwordEncoder() { 
+        return new BCryptPasswordEncoder(); 
+    } 
+} 
+ 
+ 
+ 
+
+ 
